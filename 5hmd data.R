6848aa1ff2e73460cf32361bb3fbfc339affa76f
el-lshtm <eleanor.lucas1@student.lshtm.ac.uk>
2026-09-20 21:40:37 +0100

    5hmd data.R

--- 5hmd data.R
+++ 5hmd data.R
@@ -9,8 +9,8 @@ library(ggplot2)
 library(rio)
 library(HMDHFDplus)
 
-myHMDusername <- "eleanor.lucas1@student.lshtm.ac.uk"
-myHMDpassword <- "CYt$d3fR5_XQ8ih"
+myHMDusername <- "insert-user"
+myHMDpassword <- "insert-password"
 
 #importing m and f lifetables for each country then joining them
 
